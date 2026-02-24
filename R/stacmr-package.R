a9323b4b6a37a8a624eec77cf2f2395fc4dc2c54
Johannes Titz <johannes@titz.science>
2026-02-24 12:06:04 +0100

    fix: add dependencies correctly

---
+++ R/stacmr-package.R
@@ -0,0 +1,24 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 coord_fixed
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 element_line
+#' @importFrom ggplot2 element_rect
+#' @importFrom ggplot2 element_text
+#' @importFrom ggplot2 geom_errorbar
+#' @importFrom ggplot2 geom_errorbarh
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 scale_fill_brewer
+#' @importFrom ggplot2 theme
+#' @importFrom methods is
+#' @importFrom stats cov
+#' @importFrom stats var
+#' @importFrom stats variable.names
+## usethis namespace: end
+NULL
